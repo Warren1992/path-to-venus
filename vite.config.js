af6cdcdf2be24be09deb1ef0c21d0c86c4524688
Warren1992 <warrenkonitshek@gmail.com>
2026-05-08 11:26:56 -0500

    Fix merge conflict

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/path-to-venus/',
   plugins: [react(), tailwindcss()],
 })
