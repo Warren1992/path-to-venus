e576d753962f313440f53e64a942f6583d599a15
Warren1992 <warrenkonitshek@gmail.com>
2026-05-24 11:15:14 -0500

    Fix custom domain base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/path-to-venus/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
